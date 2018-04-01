fddfe6c89184d7b5a381cec21e2955ad41a33820
Justin Song <jsong97@outlook.com>
2018-04-01 10:40:42 +1000

    adding stories

--- public/js/main.js
+++ public/js/main.js
@@ -1,18 +1,1 @@
-$(document).ready(function(){
-  $('.delete-article').on('click', function(e){
-    $target = $(e.target);
-    const id = $target.attr('data-id');
-    console.log(id);
-    $.ajax({
-      type:'DELETE',
-      url:'/articles/'+id,
-      success: function(response){
-        alert('Deleting Article');
-        window.location.href='/';
-      },
-      error: function(err){
-        console.log(err);
-      }
-    });
-  });
-});
+
--- routes/stories.js
+++ routes/stories.js
@@ -5,5 +5,9 @@ router.get('/', function(req, res){
   res.render('stories');
 });
 
+router.get('/:id', function(req, res){
+  res.render('story');
+});
+
 
 module.exports = router;
