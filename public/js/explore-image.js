0963760ff990a383e41d33b7d170bcc5eb173159
Justin Song <jsong97@outlook.com>
2018-04-18 15:26:36 +1000

    numbers move!

--- public/js/explore-image.js
+++ public/js/explore-image.js
@@ -1,8 +1,25 @@
 let sliderImages = document.querySelectorAll('.slide');
-
 let arrowLeft = document.querySelector('#arrow-left');
-
 let arrowRight = document.querySelector('#arrow-right');
+
+let updatingUsers = document.querySelector('.num-users');
+let updatingCompleted = document.querySelector('.num-completed');
+let updatingDeveloping = document.querySelector('.num-developing');
+
+var up = true;
+var users_value = 0;
+var completed_value = 2340;
+var developing_value = 10123;
+
+function PerformCalc() {
+  updatingUsers.innerHTML = 'Users: ' + users_value + '<br />';
+  updatingCompleted.innerHTML = 'Completed: ' + completed_value + '<br />';
+  updatingDeveloping.innerHTML = 'Developing: ' + developing_value + '<br />';
+}
+setInterval(PerformCalc, 1000);
+setInterval(function(){users_value+=1}, 10000);
+setInterval(function(){completed_value+=1}, 5000);
+setInterval(function(){developing_value+=1}, 2000);
 
 let current = 0;
 
