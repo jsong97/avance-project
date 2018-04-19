c216cba0f3e0d47565b6a798fcde85b35276f6cb
KathrynTeo <kteo1@student.unimelb.edu.au>
2018-04-19 21:21:33 +1000

    added My Dashboard in navbar when route to users/id/id

--- routes/users.js
+++ routes/users.js
@@ -9,19 +9,19 @@ router.get('/register', function(req, res){
   res.render('register');
 });
 
-router.get('/:id/myprojects', function(req, res){
+router.get('/:id', function(req, res){
   res.render('myprojects');
 });
 
-router.get('/:id/myprojects/:id', function(req, res){
+router.get('/:id/:id', function(req, res){
   res.render('project');
 });
 
-router.get('/:id/myprojects/:id/upload', function(req, res){
+router.get('/:id/:id/upload', function(req, res){
   res.render('upload');
 });
 
-router.get('/:id/myprojects/:id/:id', function(req, res){
+router.get('/:id/:id/:id', function(req, res){
   res.render('project-image');
 });
 
