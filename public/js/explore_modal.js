a7b348c9f42a8a85eeaa1ced6bc7b5d246b8bd37
Justin Song <jsong97@outlook.com>
2018-04-24 18:12:14 +1000

    got modal to work across all images

--- public/js/explore_modal.js
+++ public/js/explore_modal.js
@@ -1,15 +1,28 @@
 var modal = document.getElementById('myModal');
 
 // Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+var all_projects = document.querySelectorAll("#myBtn");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks the button, open the modal
-btn.onclick = function() {
+
+all_projects.forEach(function(project){
+  // // will implement this when we have databases
+  // project.addEventListener('click', function(){
+  //
+  //   // var project_id = this.getAttribute('project-id');
+  // })
+
+  project.onclick = function() {
     modal.style.display = "block";
-}
+  }
+});
+
+// btn.onclick = function() {
+//     modal.style.display = "block";
+// }
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
