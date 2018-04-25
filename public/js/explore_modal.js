6d09bc3f99c8c59664980cd177e740d445562ca9
Justin Song <jsong97@outlook.com>
2018-04-25 16:58:03 +1000

    added dynamic Modal

--- public/js/explore_modal.js
+++ public/js/explore_modal.js
@@ -1,38 +1,6 @@
-var modal = document.getElementById('myModal');
-
-// Get the button that opens the modal
-var all_projects = document.querySelectorAll("#myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal
-
-all_projects.forEach(function(project){
-  // // will implement this when we have databases
-  // project.addEventListener('click', function(){
-  //
-  //   var project_id = this.getAttribute('project-id');
-  //   ... in the onclick function you display the right content
-  // })
-
-  project.onclick = function() {
-    modal.style.display = "block";
-  }
+$('.openBtn').on('click',function(){
+  $('.modal-body').load('/explore/5/5',function(){
+    $('#myModal').modal({show:true});
+    console.log("hi");
+  });
 });
-
-// btn.onclick = function() {
-//     modal.style.display = "block";
-// }
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
