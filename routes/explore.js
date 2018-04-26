a313a1845333ca878e6345947e15e1e4747af027
Justin Song <jsong97@outlook.com>
2018-04-26 16:37:25 +1000

    made 9 new projects for explore page, fully functioning

--- routes/explore.js
+++ routes/explore.js
@@ -21,6 +21,45 @@ router.get('/:id', function(req, res){
   res.render('other-user');
 });
 
+// all the different projects ////////////////
+router.get('/1/1', function(req, res){
+  res.render('./demo_projects/user_1_project');
+});
+
+router.get('/1/2', function(req, res){
+  res.render('./demo_projects/user_2_project');
+});
+
+router.get('/1/3', function(req, res){
+  res.render('./demo_projects/user_3_project');
+});
+
+router.get('/1/4', function(req, res){
+  res.render('./demo_projects/user_4_project');
+});
+
+router.get('/1/5', function(req, res){
+  res.render('./demo_projects/user_5_project');
+});
+
+router.get('/1/6', function(req, res){
+  res.render('./demo_projects/user_6_project');
+});
+
+router.get('/1/7', function(req, res){
+  res.render('./demo_projects/user_7_project');
+});
+
+router.get('/1/8', function(req, res){
+  res.render('./demo_projects/user_8_project');
+});
+
+router.get('/1/9', function(req, res){
+  res.render('./demo_projects/user_9_project');
+});
+
+//////////////////////////////////////////////
+
 router.get('/:id/:id', function(req, res){
   res.render('other-user-project');
 });
