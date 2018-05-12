b8510aaece43803092d708c9455546e76eacdd46
KathrynTeo <37195072+KathrynTeo@users.noreply.github.com>
2018-05-12 20:56:17 +1000

    change back to string

--- models/project_image.js
+++ models/project_image.js
@@ -4,7 +4,7 @@ var projectImageSchema = new mongoose.Schema(
     {
         "filename": {type:String, required:true},
         "name": {type: String, required:true},
-        "uploadTime": {type: Date, required: true},
+        "uploadTime": {type: String, required: true},
         //"imageData": {type: Buffer, required: true},
         "imageDescription": {type: String, required:true},
         "project_id": {type: String, required: true}
